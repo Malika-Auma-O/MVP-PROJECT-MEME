8b5f9755e56b395835212e6c991dcd9ce1b41cf1
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-07-02 07:00:01 +0300

    added href in users, and navigate in the Memes array to prevent fetch

--- backend-server/modules/memes.js
+++ backend-server/modules/memes.js
@@ -5,9 +5,9 @@ const db = require("./connection");
 const MemesSchema = mongoose.Schema({
     // randomId: mongoose.Schema.Types.ObjectId,
     userId: String,
-    url: String,
     topText: String,
     bottomText: String,
+    url: String,
   });
 
   const MemesModel = mongoose.model("Memes", MemesSchema);
